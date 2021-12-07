09b7537c84fbede738992b6ecfd1814be1be6ceb
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-07 18:56:52 -0500

    add model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=100 )
+    model = models.CharField(max_length=100 )
+    year = models.IntegerField()
+    description = models.CharField(max_length=250 )
+
+    def __str__(self):
+        return self.make
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-class Car:
-    def __init__(self, make, model, year, description):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.description = description
-    
-cars = [
-     Car('Audi', 'Q5', '2022', '2022 Audi Q5 2.0T quattro Premium Plus S Line AWD'),
-     Car('Hyundai', 'Elantra', '2021', 'Amazing car'),
-     Car('Hyundai', 'Accent', '2022', 'Hyundai Accent Preferred Hatchback FWD'),
-]
-
+from .models import Car
 
 def home(request):
     return render(request, 'home.html')
@@ -25,4 +10,5 @@ def about(request):
 
 # Add new view
 def cars_index(request):
-  return render(request, 'cars/index.html', { 'cars': cars })
+    cars = Car.objects.all()
+    return render(request, 'cars/index.html', { 'cars': cars })
