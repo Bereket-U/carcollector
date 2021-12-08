84cf47d059cb43ff12486c69396cce3f4219f19f
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-07 19:38:43 -0500

    add detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name ='about'),
-      # route for cats index
+      # route for cars index
     path('cars/', views.cars_index, name='index'),
+    path('cars/<int:car_id>/', views.cars_detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,7 @@ def about(request):
 def cars_index(request):
     cars = Car.objects.all()
     return render(request, 'cars/index.html', { 'cars': cars })
+
+def cars_detail(request, car_id):
+    car = Car.objects.get(id = car_id)
+    return render(request, 'cars/detail.html', {'car': car})
