2c5790e819e979c1378a0f93cffcf3ac7365e76b
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-10 01:44:44 -0500

    Add service functionality - add service view  on detail page

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Service
+
 # Register your models here.
 admin.site.register(Car)
+admin.site.register(Service)
--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 from django.urls import reverse
 
-# Create your models here.
+SERVICES = (
+  ('BS', 'Basic Service'),
+  ('OC', 'Oil Change'),
+  ('RAF', 'Replace air filter'),
+  ('BR', 'Battery replacement'),
+  ('WB', 'Wheels align/balance'),
+  ('AF', 'Antifreeze added')
+)
+
 
 class Car(models.Model):
     make = models.CharField(max_length=100 )
@@ -14,3 +23,13 @@ class Car(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'car_id': self.id})
+
+# Add new Service model
+class Service(models.Model):
+  date = models.DateField('Service date')
+  service = models.CharField(max_length=3, choices=SERVICES, default=[0][0])
+  # Create a car_id FK
+  car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return f"{self.get_service_display()} on {self.date}"
