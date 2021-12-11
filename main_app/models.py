3e798efccd8212b1fca8c66298e3372307a6357f
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-11 18:37:40 -0500

    Add accessory model

--- main_app/models.py
+++ main_app/models.py
@@ -11,12 +11,23 @@ SERVICES = (
   ('AF', 'Antifreeze added')
 )
 
+class Accessory(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('accessories_detail', kwargs={'pk': self.id})
+
 
 class Car(models.Model):
     make = models.CharField(max_length=100 )
     model = models.CharField(max_length=100 )
     year = models.IntegerField()
     description = models.TextField(max_length=250 )
+    # Add the M:M relationship with Accessory
+    accessories = models.ManyToManyField(Accessory)
 
     def __str__(self):
         return self.make
