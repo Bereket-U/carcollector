205ca23a52d4d448eb67da533c5c6ead10547c2e
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-11 18:24:45 -0500

    update models description to TextField

--- main_app/models.py
+++ main_app/models.py
@@ -16,7 +16,7 @@ class Car(models.Model):
     make = models.CharField(max_length=100 )
     model = models.CharField(max_length=100 )
     year = models.IntegerField()
-    description = models.CharField(max_length=250 )
+    description = models.TextField(max_length=250 )
 
     def __str__(self):
         return self.make
