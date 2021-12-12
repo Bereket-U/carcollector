c9cba6a5a8d965b9dc6742e68bd8b9981e85d874
Bereket Urgessa <bereketadugna15@gmail.com>
2021-12-11 21:16:09 -0500

    register Accessories on admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Car, Service
+from .models import Car, Service, Accessory
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Service)
+admin.site.register(Accessory)
