46932164fdc8997a95b38cef9a93f29455a69d9f
Bereket Urgessa <bereketadugna15@gmail.com>
2022-01-03 19:57:01 -0500

    update header and footer - fixed

--- carcollector/settings.py
+++ carcollector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = 'django-insecure-+-*zx#3@%df$wm29nj)^5li##mtu_mqd26s%u_-d&@08ggce5v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
